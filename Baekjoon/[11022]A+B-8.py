3ed3a586f5be70b1d097928d258091c2de6021eb
Chaaaaabbo <cbkyeong@gmail.com>
2022-11-02 23:23:52 +0900

    11/2 baekjoon

---
+++ Baekjoon/[11022]A+B-8.py
@@ -0,0 +1,9 @@
+'''
+- 문제 링크 : https://www.acmicpc.net/problem/11022
+'''
+
+import sys
+
+for i in range(int(input())):
+    a,b = map(int, sys.stdin.readline().split())
+    print(f'Case #{i+1}: {a} + {b} = {a+b}')
---
+++ Baekjoon/[15552]fast_A+B.py
@@ -0,0 +1,9 @@
+'''
+- 문제 링크 : https://www.acmicpc.net/problem/15552
+'''
+
+import sys
+
+for i in range(int(input())):
+    a, b = map(int, sys.stdin.readline().split())
+    print(a + b)
