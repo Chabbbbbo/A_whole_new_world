02e57f1d12aa62a815b2d45c7cea01d88d29a237
Chabbo <cbkyeong@gmail.com>
2023-03-08 23:46:14 +0900

    검색어 조건있는 경우 추가

--- Crawling_system.py
+++ Crawling_system.py
@@ -59,13 +59,14 @@ class CrawlingSystem():
     def fm_crawling(self, driver:webdriver, search_word: str, related_word: str):
         '''
         /www.fmkorea.com 크롤링
+        검색어 조건 없음
         '''
+        search_list = []
         url = f'https://www.fmkorea.com/search.php?act=IS&is_keyword={search_word}+{related_word}'
         driver.get(url)
         time.sleep(1)
         soup = BeautifulSoup(driver.page_source, 'html.parser')
         search_in_commu = soup.find('ul', class_="searchResult")
-        search_list = []
         for search in search_in_commu: 
             search_dict = dict()
             if search != ' ':
@@ -91,28 +92,37 @@ class CrawlingSystem():
     def pp_crawling(self, driver:webdriver, search_word: str, related_word: str):
         '''
         www.ppomppu.co.kr 크롤링
+        검색어 둘 중 한개라도 한글2자, 영어 4자 이하면 검색 안됨
+        특수한글 취급X (쉪,뷁,..)
         '''
-        url = f'https://www.ppomppu.co.kr/search_bbs.php?bbs_cate=2&keyword={search_word}+{related_word}'
-        driver.get(url)
-        time.sleep(1)
-        soup = BeautifulSoup(driver.page_source, 'html.parser')
-        search_in_commu = soup.find_all('div', class_="content")
-        search_list = []
-        for search in search_in_commu: 
-            search_dict = dict()
-            comment_cnt = int(search.find('font', class_ = 'comment-cnt').get_text())
-            search.find('a').font.decompose()
-            title = search.find('a').get_text()
-            url = f"https://www.ppomppu.co.kr{search.find('a')['href']}"
-            date = search.find('p', class_ = 'desc').select_one('span:nth-child(3)').string.split('.')
-            date = '-'.join(date) # yyyy-mm-dd 형식 맞춰줌
-            search_dict['search_word'] = search_word
-            search_dict['related_word'] = related_word
-            search_dict['title'] = title
-            search_dict['url'] = url
-            search_dict['date'] = date
-            search_dict['comment_cnt'] = comment_cnt
-            search_list.append(search_dict)
+        if (len(bytes(search_word, 'euc-kr')) < 4) or (len(bytes(related_word, 'euc-kr')) < 4):
+            return 
+        elif len(search_word) *2 != len(bytes(search_word, 'euc-kr')) or len(related_word) *2 != len(bytes(related_word, 'euc-kr')):
+            if re.match("^[가-힣]+$", (search_word and related_word)):
+                if len(search_word) ==2 or len(related_word) == 2:
+                    return
+        else:
+            search_list = []
+            url = f'https://www.ppomppu.co.kr/search_bbs.php?bbs_cate=2&keyword={search_word}+{related_word}'
+            driver.get(url)
+            time.sleep(1)
+            soup = BeautifulSoup(driver.page_source, 'html.parser')
+            search_in_commu = soup.find_all('div', class_="content")
+            for search in search_in_commu: 
+                search_dict = dict()
+                comment_cnt = int(search.find('font', class_ = 'comment-cnt').get_text())
+                search.find('a').font.decompose()
+                title = search.find('a').get_text()
+                url = f"https://www.ppomppu.co.kr{search.find('a')['href']}"
+                date = search.find('p', class_ = 'desc').select_one('span:nth-child(3)').string.split('.')
+                date = '-'.join(date) # yyyy-mm-dd 형식 맞춰줌
+                search_dict['search_word'] = search_word
+                search_dict['related_word'] = related_word
+                search_dict['title'] = title
+                search_dict['url'] = url
+                search_dict['date'] = date
+                search_dict['comment_cnt'] = comment_cnt
+                search_list.append(search_dict)
         return search_list
 
 
@@ -120,41 +130,52 @@ class CrawlingSystem():
         '''
         http://web.humoruniv.com 크롤링
         main site에서 검색(검색 링크에서 바로 안됨)
+        검색어 둘 중 1개라도 한글2자, 영어 4자 이하면 검색 안됨
+        특수한글 취급X (쉪,뷁,..)
         '''
-        url = "http://web.humoruniv.com/main.html" 
-        driver.get(url)
-        time.sleep(1)
+                # 탐색어 연관어 둘 중 하나라도 4byte 이하면 검색안됨 & 한글 2자일때, 하나라도 이상문자(쉪)이면 안됨
+        if (len(bytes(search_word, 'euc-kr')) < 4) or (len(bytes(related_word, 'euc-kr')) < 4):
+            return 
+        elif len(search_word) *2 != len(bytes(search_word, 'euc-kr')) or len(related_word) *2 != len(bytes(related_word, 'euc-kr')):
+            if re.match("^[가-힣]+$", (search_word and related_word)):
+                if len(search_word) ==2 or len(related_word) == 2:
+                    return
+        else:
+            search_list = []
+            url = "http://web.humoruniv.com/main.html" 
+            driver.get(url)
+            time.sleep(1)
 
-        x_path_search = '//*[@id="search_text"]'
-        searchbox = driver.find_element(By.XPATH, x_path_search)
-        searchbox.click()
-        element = driver.find_element(By.NAME, "search_text")
-        element.send_keys(f'{search_word} {related_word}')
-        element.submit()
+            x_path_search = '//*[@id="search_text"]'
+            searchbox = driver.find_element(By.XPATH, x_path_search)
+            searchbox.click()
+            element = driver.find_element(By.NAME, "search_text")
+            element.send_keys(f'{search_word} {related_word}')
+            element.submit()
 
-        soup = BeautifulSoup(driver.page_source, 'html.parser')
-        search_in_commu = soup.find_all('td', style ="word-break: break-all;")
-        search_list = []
-        for search in search_in_commu: 
-            search_dict = dict()
-            title = search.find('a').get_text()
-            url = search.find('a')['href']
-            date = search.find('font').get_text().split(' ')[0]
-            comment_cnt = search.find('font', color = 'green').get_text().split(' ')[1]
+            soup = BeautifulSoup(driver.page_source, 'html.parser')
+            search_in_commu = soup.find_all('td', style ="word-break: break-all;")
+            for search in search_in_commu: 
+                search_dict = dict()
+                title = search.find('a').get_text()
+                url = search.find('a')['href']
+                date = search.find('font').get_text().split(' ')[0]
+                comment_cnt = search.find('font', color = 'green').get_text().split(' ')[1]
 
-            search_dict['search_word'] = search_word
-            search_dict['related_word'] = related_word
-            search_dict['title'] = title
-            search_dict['url'] = url
-            search_dict['date'] = date
-            search_dict['comment_cnt'] = comment_cnt
-            search_list.append(search_dict)
+                search_dict['search_word'] = search_word
+                search_dict['related_word'] = related_word
+                search_dict['title'] = title
+                search_dict['url'] = url
+                search_dict['date'] = date
+                search_dict['comment_cnt'] = comment_cnt
+                search_list.append(search_dict)
         return search_list
 
     
     def mp_crawling(self, driver:webdriver, search_word: str, related_word: str):
         '''
         https://mlbpark.donga.com 크롤링
+        검색어 조건 X
         '''
         url = f'https://mlbpark.donga.com/mp/b.php?search_select=sct&search_input={search_word}+{related_word}&x=0&y=0&select=sct&m=search&b=bullpen&query={search_word}+{related_word}'
         driver.get(url)
@@ -194,28 +215,29 @@ def word_load(path):
 
 def main():
     start = time.time()
-    path = "C:\\Users\\Chaaaaabbo\\Desktop\\similar_word_data_20230224.json"
+    path = "*"
     json_data = word_load(path)
     print(f"json_data 로드시간 =>>> {time.time() - start}")
     crawling_system = CrawlingSystem()
     driver = crawling_system.setup_chrome_driver()
     start = time.time()
     crawling_data = []
-    for idx, search_word in enumerate(json_data):
-        if idx == 1:
-            for related_word in json_data[search_word]:
-                if re.match("^[가-힣a-zA-Z0-9 ]+$", (search_word and related_word)):
-                    search_list = crawling_system.pp_crawling(driver, search_word, related_word)
-                    crawling_data.append(search_list)
-                    search_list = crawling_system.fm_crawling(driver, search_word, related_word)
-                    crawling_data.append(search_list)
-                    search_list = crawling_system.hu_crawling(driver, search_word, related_word)
-                    crawling_data.append(search_list)
-                    search_list = crawling_system.mp_crawling(driver, search_word, related_word)
-                    crawling_data.append(search_list)
+    for search_word in json_data:
+        word_time = time.time()
+        for related_word in json_data[search_word]:
+            if re.match("^[가-힣a-zA-Z0-9_ ]+$", (search_word and related_word)):
+                search_list = crawling_system.pp_crawling(driver, search_word, related_word)
+                crawling_data.append(search_list)
+                search_list = crawling_system.fm_crawling(driver, search_word, related_word)
+                crawling_data.append(search_list)
+                search_list = crawling_system.hu_crawling(driver, search_word, related_word)
+                crawling_data.append(search_list)
+                search_list = crawling_system.mp_crawling(driver, search_word, related_word)
+                crawling_data.append(search_list)
             break
+        print(f'{search_word} search fin ==> {time.time() - word_time}sec')
     crawling_df = pd.DataFrame(list(chain.from_iterable(crawling_data)))
-    print(f"search_word 검색시간 =>>> {time.time() - start}")
+    print(f"{len(crawling_df)} 검색시간 =>>> {time.time() - start}")
     print(crawling_df)
     return crawling_df
 
@@ -223,4 +245,4 @@ def main():
 
 if __name__ == "__main__":
 	main()
-        ## search_word 검색시간 =>>> 127.47889471054077 조합 10개 [654 rows x 6 columns]
+        ## search_word 검색시간 =>>> search_word 하나에 평균 100 sec
